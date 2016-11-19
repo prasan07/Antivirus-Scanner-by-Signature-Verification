578b775279e2b9079e6066b36ec04d1d67c92354
barani008 <barani008@gmail.com>
2016-11-19 17:32:06 -0500

    Need to add transaction support and table clean and add new values feature.

---
+++ barani_changes/dbutility.h
@@ -0,0 +1,29 @@
+#ifndef db_utility_H
+#define db_utility_H
+
+#define PAGE_SIZE 4096
+
+#define REMOTE_LOC "172.24.22.73"
+#define REMOTE_USER "barani008"
+#define SERVER_LOC "localhost"
+#define USER "root"
+#define PASS "criminalminds"
+#define DATABASE "cse509"
+
+struct signatures{
+unsigned int sig_count;
+unsigned char * signatures;
+};
+
+//Method to retrieve the sha256 hash of the file given the path
+extern char* getsha256(char * file_path);
+
+//method to retrieve the list of virus signatures
+extern struct signatures* getstructures();
+
+//method to update the virus signatures
+extern int update_structures();
+
+//method to compare the whitelist hashes
+extern int isWhitelisted(char * file_path);
+#endif
