7c5285fb2fc43ba6460d85ae0fb5cfa357ef4136
Prasanth <prsan.sridr@gmail.com>
2016-11-21 14:49:02 -0500

    malloc validation

--- Pras_changes/blacklist.c
+++ Pras_changes/blacklist.c
@@ -26,7 +26,7 @@ int blacklist_scan(char* file_path){
 	/* Perform whitelist validation, call made to DB API " */
 	ret = isWhitelisted(file_path);
 	if( ret < 0){
-#ifdef ERR
+#ifdef DEBUG
 		fprintf(stderr, "Error during whitelist validation");
 #endif
 		goto exit_fn;
@@ -40,14 +40,14 @@ int blacklist_scan(char* file_path){
         } else {
                 f = open(file_path, O_RDONLY, 0);
                 if(f < 0){
-#ifdef ERR
+#ifdef DEBUG
                         perror(file_path);
 #endif
                         ret = -1;
                         goto exit_fn;
                 }
                 if(stat(file_path,&st) < 0){
-#ifdef ERR
+#ifdef DEBUG
                         perror(file_path);
 #endif
                         ret = -1;
@@ -64,7 +64,13 @@ int blacklist_scan(char* file_path){
                 blacklist = getstructures();
                 fsize = st.st_size;
                 file_bytes = malloc(fsize+1);
-                // Malloc fail add here
+                if(!file_bytes){
+#ifdef DEBUG
+			fprintf(stderr, "Memory allocation error ");		
+#endif
+			ret = -1;
+			goto exit_fn;
+		}
 
                 i = read(f, file_bytes, fsize);
                 *(file_bytes + fsize) = '\0';
@@ -73,7 +79,7 @@ int blacklist_scan(char* file_path){
 #ifdef ERR	
                         fprintf(stderr,"File read error");
 #endif
-                        close(f);
+                        ret = -1;
                         goto exit_fn;
                 }
                 else if(i < fsize){
@@ -81,39 +87,14 @@ int blacklist_scan(char* file_path){
                         fprintf(stdout, "Only partial file read of %s ",file_path);
 #endif
                         ret = -1;
-                        close(f);
                         goto exit_fn;
                 }
                 else{		
-                        /* unsigned char* hex_str = malloc(2*fsize + 1);
-                         *(hex_str + 2*fsize) = '\0';
-                         unsigned char* copy_prt = hex_str;
-                         for (i = 0; i < size; i++)
-                         {
-                         copy_ptr += sprintf(copyfb_ptr, "%02X", *(buf+i));
-                         }*/
 
                         for(i = 1; i <= blacklist->sig_count; i++){
                                 int j = 0;
                                 signature = blacklist->signatures + next_pos;
-                                #if 0
-                                printf("%s %ld ---- %s\n", file_bytes, strlen(file_bytes), signature);
-                                if(strstr(file_bytes, signature) != NULL){
-                                        ret = 1;
-#ifdef DEBUG
-                                        fprintf(stdout, "%s is a virus ",file_path);
-#endif
-                                        goto exit_fn;
-                                }
-                                #endif
-                                for (j = 0; j < (fsize - strlen(signature - 1)); j++) {
-                                        #if 0
-                                        char *temp = malloc(strlen(signature) + 1);
-                                        memcpy(temp, file_bytes + j, strlen(signature));
-                                        temp[strlen(signature)] = '\0';
-                                        
-                                        printf("===> Read string %s\n", temp);
-                                        #endif
+                                for (j = 0; j <= (fsize - strlen(signature)); j++) {
                                         if (memcmp(file_bytes + j, signature, strlen(signature)) == 0) {
                                                 ret = 1;
                                                 goto exit_fn;
@@ -131,8 +112,10 @@ int blacklist_scan(char* file_path){
 #ifdef DEBUF
 	fprintf(stdout, "%s is not a virus ",file_path);
 #endif
-	close(f);
-	exit_fn:
-		return ret;
+exit_fn:
+	if(f>=0){
+		close(f);
+	}
+	return ret;
 }
 
