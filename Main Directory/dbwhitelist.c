4a1bfed8f33eed264d8a0fdae3ca9b15b49a7cff
shyamsundarc92 <shychandrase@cs.stonybrook.edu>
2016-11-26 20:17:53 -0500

    Fixed index

--- Main Directory/dbwhitelist.c
+++ Main Directory/dbwhitelist.c
@@ -15,27 +15,23 @@ int insertWhiteList(char * file_path){
 	MYSQL * conn;
 
 	conn = mysql_init(NULL);
-	printf("Connection\n");
         // Connect to local database 
         if (!mysql_real_connect(conn, SERVER_LOC,
                                 USER, PASS, DATABASE, 0, NULL, 0)) {
                 fprintf(stderr, "%s\n", mysql_error(conn));
                 return -1;
         }
-	printf("Connection1\n");
 	//verify tables exist
 	if(verify_tables(conn)!=0){
 		fprintf(stderr, "%s\n", mysql_error(conn));
                 return -1;
 	}
-	printf("Connection2\n");
 	//start Transaction
 	if(mysql_query(conn, "START TRANSACTION")){
                 fprintf(stderr, "%s\n", mysql_error(conn));
                 result = -1;
                 goto out;
         }
-	printf("Connection3\n");
 	if ((dir = opendir (file_path)) != NULL) {
                 while ((ent = readdir (dir)) != NULL) {
                         sprintf (filename, "%s/%s", file_path, ent->d_name);
@@ -51,7 +47,6 @@ int insertWhiteList(char * file_path){
                 }
                 closedir (dir);
         }
-	printf("Connection4\n");
 	if(mysql_query(conn, "COMMIT")){
                 fprintf(stderr, "%s\n", mysql_error(conn));
                 result = -1;
@@ -71,8 +66,9 @@ int main(){
 	DIR *dir;
 	int i=0;
 	char* whitelistDirs[6] = {"/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "/bin"};
-	while ((dir = opendir (whitelistDirs[i])) != NULL) {
+	while ((i < 6) && (dir = opendir (whitelistDirs[i])) != NULL) {
 		insertWhiteList(whitelistDirs[i]);
 		i++;
 	}
+        return 0;
 }
