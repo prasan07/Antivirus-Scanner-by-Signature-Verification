481b884245b0b7764482bf288730f7c6cfbc7c78
shyamsundarc92 <shychandrase@cs.stonybrook.edu>
2016-11-27 02:08:53 -0500

    whitelist code with comments

--- Main Directory/dbwhitelist.c
+++ Main Directory/dbwhitelist.c
@@ -5,37 +5,28 @@
 #include "dbutility.h"
 #include <mysql.h>
 
-int insertWhiteList(char * file_path){
+int insertWhiteList(MYSQL *conn, char * file_path){
 	char* hash_val = NULL;
 	DIR *dir;
         struct dirent *ent;
 	char query[300];
 	char filename[256];
 	int result = 0;
-	MYSQL * conn;
-
-	conn = mysql_init(NULL);
-        // Connect to local database 
-        if (!mysql_real_connect(conn, SERVER_LOC,
-                                USER, PASS, DATABASE, 0, NULL, 0)) {
-                fprintf(stderr, "%s\n", mysql_error(conn));
-                return -1;
-        }
-	//verify tables exist
-	if(verify_tables(conn)!=0){
-		fprintf(stderr, "%s\n", mysql_error(conn));
-                return -1;
-	}
+	
 	//start Transaction
 	if(mysql_query(conn, "START TRANSACTION")){
+#ifdef DEBUG
                 fprintf(stderr, "%s\n", mysql_error(conn));
+#endif
                 result = -1;
                 goto out;
         }
 	if ((dir = opendir (file_path)) != NULL) {
                 while ((ent = readdir (dir)) != NULL) {
                         sprintf (filename, "%s/%s", file_path, ent->d_name);
+#ifdef DEBUG
                         printf("%s \n", filename);
+#endif
                         //retrieve hash of the given file
         		hash_val = getsha256(filename);
         		sprintf(query, "insert into whitelist (hash) values ('%s')", hash_val);
@@ -48,17 +39,19 @@ int insertWhiteList(char * file_path){
                 closedir (dir);
         }
 	if(mysql_query(conn, "COMMIT")){
+#ifdef DEBUG
                 fprintf(stderr, "%s\n", mysql_error(conn));
+#endif
                 result = -1;
         }
         //perform clean up and exit
 out:
         if(result == -1){
                 mysql_query(conn, "ROLLBACK");
+#ifdef DEBUG
                 printf("Rolling back DB changes\n");
+#endif
         }
-	if(conn!=NULL)
-		mysql_close(conn);
 	return result;
 }
 
@@ -66,9 +59,31 @@ int main(){
 	DIR *dir;
 	int i=0;
 	char* whitelistDirs[6] = {"/usr/local/sbin", "/usr/local/bin", "/usr/sbin", "/usr/bin", "/sbin", "/bin"};
-	while ((i < 6) && (dir = opendir (whitelistDirs[i])) != NULL) {
-		insertWhiteList(whitelistDirs[i]);
+	MYSQL *conn;
+
+        conn = mysql_init(NULL);
+        //Connect to local database 
+        if(!mysql_real_connect(conn, SERVER_LOC,
+                                USER, PASS, DATABASE, 0, NULL, 0)) {
+#ifdef DEBUG
+                fprintf(stderr, "%s\n", mysql_error(conn));
+#endif
+                return -1;
+        }
+        //verify tables exist
+        if(verify_tables(conn)!=0){
+#ifdef DEBUG
+                fprintf(stderr, "%s\n", mysql_error(conn));
+#endif
+                return -1;
+        }
+        
+        while ((i < 6) && (dir = opendir (whitelistDirs[i])) != NULL) {
+		insertWhiteList(conn, whitelistDirs[i]);
 		i++;
 	}
+        if(conn!=NULL)
+        mysql_close(conn);
+
         return 0;
 }
