80f8bf8efbdb1ef6fa4e76dca147805f8ad55c14
prasanth <prasan.sridr@gmail.com>
2016-11-27 20:42:58 -0500

    progs_compile_error

--- Main Directory/test_programs/c_progs/backtrack.c
+++ Main Directory/test_programs/c_progs/backtrack.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<math.h>
- 
+#include<stdlib.h> 
 char a[10][10];
 int n;
  
--- Main Directory/test_programs/c_progs/fileops.c
+++ Main Directory/test_programs/c_progs/fileops.c
@@ -5,20 +5,18 @@ int main() {
    char fname1[40], fname2[40];
  
    printf("Enter name of first file :");
-   gets(fname1);
+   fgets(fname1,256,stdin);
  
    printf("Enter name of second file:");
-   gets(fname2);
+   fgets(fname2,256,stdin);
  
    fp1 = fopen(fname1, "r");
    fp2 = fopen(fname2, "r");
  
    if (fp1 == NULL) {
       printf("Cannot open %s for reading ", fname1);
-      exit(1);
    } else if (fp2 == NULL) {
       printf("Cannot open %s for reading ", fname2);
-      exit(1);
    } else {
       ch1 = getc(fp1);
       ch2 = getc(fp2);
