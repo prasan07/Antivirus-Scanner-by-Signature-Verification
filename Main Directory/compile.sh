9a5c0a2b83ae738086d98e5b4be30237aff4b30a
prasanth <prasan.sridr@gmail.com>
2016-11-27 19:46:21 -0500

    repblacklist

--- Main Directory/compile.sh
+++ Main Directory/compile.sh
@@ -1,3 +1,4 @@
 gcc -Wall -Werror antivirus.c -I/usr/include/mysql blacklist.c dboperations.c getSha256.c -lm $(mysql_config --libs) -lcrypto -lssl -o antivirus
 
 gcc -Wall -Werror dbwhitelist.c -I/usr/include/mysql getSha256.c dboperations.c -lm $(mysql_config --libs) -lcrypto -lssl -o dbwhitelist
+gcc -Wall -Werror dbblacklist.c -I/usr/include/mysql getSha256.c dboperations.c -lm $(mysql_config --libs) -lcrypto -lssl -o dbblacklist
